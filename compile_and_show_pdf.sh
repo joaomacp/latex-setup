249a3771e248ebd778e053d27cfc43b56bbdcfc9
jpereira <joaomacpereira@tecnico.ulisboa.pt>
2019-11-30 16:05:45 +0000

    Add explanatory comments

--- compile_and_show_pdf.sh
+++ compile_and_show_pdf.sh
@@ -1,3 +1,7 @@
 cd "${THESIS_PATH}"
+
+# Compile main.tex ignoring warnings
 pdflatex -interaction nonstopmode main.tex
+
+# Send SIGHUP to MuPDF viewer process, which triggers a reload
 pkill -HUP mupdf-gl
